eaf099f7cbe51ec0c05188cae050f3ac6c4f17ad
jleow <jleow@iupui.edu>
2017-06-05 16:01:32 -0400

    Add change font size function

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ function handleSubmit(ev){
 
   const colorName = f.colorName.value
   document.querySelector('h1').textContent = "Hi " + name + "!" + " Your favorite color is " + colorName + "."
-  document.querySelector('p.empty-paragraph').textContent = "The color of this sentence is now " + colorName + ". Now click your mouse over this sentence and the color will change again." 
+  document.querySelector('p.empty-paragraph').textContent = "The color of this sentence is now " + colorName + ". Now click your mouse over this sentence and the color will change again."
   document.querySelector('p.empty-paragraph').style.color = colorName
 }
 
-//Change color of the empty paragraph when the mouse is clicked over the empty paragraph.
-  document.querySelector('.empty-paragraph').onclick = changeColor;
 
-  function changeColor() {
-      document.querySelector('p.empty-paragraph').style.color = "purple";
-      return false;
-  }
+function changeColor() {
+    document.querySelector('p.empty-paragraph').style.color = "purple"
+}
+
+function changeFont() {
+  document.querySelector('p.empty-paragraph').style.fontSize = "x-large"
+}
 
   personForm.addEventListener('submit', handleSubmit)
+  personForm.addEventListener('click', changeColor)
+  personForm.addEventListener('click', changeFont)
