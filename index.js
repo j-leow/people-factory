2e03a4d220ad6cf1c2ae64edf9d425a831cdb9fc
jleow <jleow@iupui.edu>
2017-06-05 13:52:47 -0400

    Add favorite color text entry input and colorName behavior

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function handleSubmit(ev){
   ev.preventDefault()
   const f = ev.target //Gives us what we had submitted
   const name = f.personName.value
-  document.querySelector('h1').textContent = name
+  document.querySelector('h1').textContent = name //Changes the h1
+
+  const colorName = f.colorName.value
+  console.log('colorName')
 }
 
 personForm.addEventListener('submit', handleSubmit)
