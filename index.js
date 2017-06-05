39ae04ba38b4d7a49c20ff95167bc3a83aff4815
jleow <jleow@iupui.edu>
2017-06-05 14:34:58 -0400

    Test color change function. Not currently working

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ function handleSubmit(ev){
   ev.preventDefault()
   const f = ev.target //Gives us what we had submitted
   const name = f.personName.value
-  document.querySelector('h1').textContent = name //Changes the h1
+  //document.querySelector('h1').textContent = name //Changes the h1
 
   const colorName = f.colorName.value
-  console.log('colorName')
+  document.querySelector('h1').textContent = "Hi " + name + "!" + " Your favorite color is " + colorName + "."
+  document.querySelector('p.empty-paragraph').textContent = "Now click your mouse!"
 }
 
 personForm.addEventListener('submit', handleSubmit)
+
+const colorChange = document.querySelector('.empty-paragraph')
+
+function changeColor(){
+  document.body.style.backgroundColor = '#FFC926'
+}
+
+colorChange.addEventListener('click', changeColor, false)
